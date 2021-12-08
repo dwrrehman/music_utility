033f1966734612a03c3385bff52eeeb8a4b486ba
Daniel Rehman <dwrrehman@gmail.com>
2021-12-07 17:41:46 -0800

    add the keyboard synthesizer code. it works pretty well, theres some things that i want to improve, and also add the ear training utility.

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+// a C program to help me develop my relative pitch, and function as a piano/keyboard.
+
+// find more tone frequencies here:  
+//
+//	https://en.wikipedia.org/wiki/Piano_key_frequencies
+//
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_audio.h>
@@ -10,27 +16,72 @@
 #include <unistd.h>
 #include <math.h>
 
-#define tau (2.0 * M_PI)
+static const double tau = 2.0 * M_PI;
 
 
-static double my_time = 0;
-static double freq = 440;
+enum note {
 
-static double my_time2 = 0;
-static double freq2 = 220;
+	C4,
+	D4,
+	E4,
+	F4,
+	G4,
 
+	A4,
+	B4,
+	C5,
+	D5,
+	E5,
 
+	note_count,
+};
 
-static void callback(void* userdata, Uint8* stream, int len) {
+static const double frequency[] = {
+	261.6256, // C4
+	293.6648, // D4
+	329.6276, // E4
+	349.2282, // F4
+	391.9954, // G4
+	440.0000, // A4
+	493.8833, // B4
+	523.2511, // C5
+	587.3295, // D5
+	659.2551, // E5
+};
+
+static const int keybind[]= {
+	SDL_SCANCODE_A,
+	SDL_SCANCODE_S,
+	SDL_SCANCODE_D,
+	SDL_SCANCODE_F,
+	SDL_SCANCODE_G,
+	SDL_SCANCODE_H,
+	SDL_SCANCODE_J,
+	SDL_SCANCODE_K,
+	SDL_SCANCODE_L,
+	SDL_SCANCODE_SEMICOLON,
+};
+
+static bool active[note_count] = {0};
+static double time_value[note_count] = {0};
+
+static void callback(void* __attribute__((unused)) userdata, Uint8* stream, int len) {
 	int16_t* snd = (int16_t*)(stream);
 	for (int i = 0; i < len / 2; i++) {
-		snd[i] = (int16_t)(5000 * (sin(my_time) + sin(my_time2)));
-		
-		my_time += freq * tau / 44100.0;
-		if (my_time >= tau) my_time -= tau;
 
-		my_time2 += freq2 * tau / 44100.0;
-		if (my_time2 >= tau) my_time2 -= tau;
+		// super-impose all sine waves together:
+		double sum = 0.0;
+		for (int n = 0; n < note_count; n++) 
+			sum += sin(time_value[n]);
+	
+		// write value to buffer:
+		snd[i] = (int16_t)(3200 * (sum)); // amplitude should be the largest n such that n * note_count < 32,767.
+		
+		// move time_value for each note forward based on frequency:
+		for (int n = 0; n < note_count; n++) {
+			time_value[n] += (double)active[n] * frequency[n] * tau / 44100.0;
+			if (time_value[n] >= tau) time_value[n] -= tau;
+		}
 	}
 }
 
@@ -41,7 +92,6 @@ int main() {
 	if (SDL_Init(SDL_INIT_EVERYTHING)) exit(printf("SDL_Init failed: %s\n", SDL_GetError()));
 
 	SDL_AudioSpec spec, aspec; 
-
 	SDL_zero(spec);
 	spec.freq = 44100; 
 	spec.format = AUDIO_S16SYS;
@@ -50,15 +100,9 @@ int main() {
 	spec.callback = callback;
 	spec.userdata = NULL;
 
-	unsigned int id = 0;
-	if (not (id = SDL_OpenAudioDevice(NULL, 0, &spec, &aspec, SDL_AUDIO_ALLOW_ANY_CHANGE))) {
-		fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError()); exit(-1);
-	}
-
+	SDL_Window *window = SDL_CreateWindow("music utility", 0, 0, 0, 0, 0);
+	const unsigned int id = SDL_OpenAudioDevice(NULL, 0, &spec, &aspec, SDL_AUDIO_ALLOW_ANY_CHANGE);
 	SDL_PauseAudioDevice(id, 0);
-
-	
-	SDL_Window *window = SDL_CreateWindow("block game", 0, 0, 0, 0, 0);
 
 	bool quit = false;
 
@@ -77,23 +121,17 @@ int main() {
 
 		const Uint8* key = SDL_GetKeyboardState(0);
 
-		if (key[SDL_SCANCODE_A]) { freq = 440; freq2 = 261.6256; }
-	          
-		if (key[SDL_SCANCODE_W]) { freq = 440; freq2 = 220; }
-
-		if (key[SDL_SCANCODE_0]) { freq = 0; freq2 = 0; }
-
-		if (key[SDL_SCANCODE_E]) { freq = 0; }
-		if (key[SDL_SCANCODE_F]) { freq2 = 0; }
-
-          //if needed, you can do cool stuff here, like change frequency for different notes: 
-          //https://en.wikipedia.org/wiki/Piano_key_frequencies
-
+		for (int n = 0; n < note_count; n++) 
+			active[n] = !!key[keybind[n]];
+		// if (key[SDL_SCANCODE_A]) { freq = 440; freq2 = 261.6256; }
+		// if (key[SDL_SCANCODE_W]) { freq = 440; freq2 = 220; }
+		// if (key[SDL_SCANCODE_0]) { freq = 0; freq2 = 0; }
+		// if (key[SDL_SCANCODE_E]) { freq = 0; }
+		// if (key[SDL_SCANCODE_F]) { freq2 = 0; }
+		
 	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
-
-
-
-
-
 
